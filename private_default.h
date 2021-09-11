d85fb48e38a4f0f7f15afe08bf0dcc7c09e10192
Zheng-Xiang Ke <kf_ke@hikingbook.net>
2021-09-11 18:09:46 +0800

    Update DEFAULT_URLS_JSON

--- private_default.h
+++ private_default.h
@@ -5,7 +5,7 @@
 #define MWM_GEOLOCATION_SERVER ""
 #define METASERVER_URL "https://hikingbook.net/maps/cdns"
 #define DIFF_LIST_URL ""
-#define DEFAULT_URLS_JSON "[\"https://hikingbook.net/\", \"https://cdn3.mapswithme.com/\", \"https://cdn2.mapswithme.com/\", \"https://v2s-2.mapswithme.com/\" \"https://v2s-1.mapswithme.com/\", \"https://maps-dl-us1.mapswithme.com/\", \"https://maps-dl-eu2.mapswithme.com/\", \"https://maps-dl-ru3.mapswithme.com/\", \"https://maps-dl-ru2.mapswithme.com/\", \"https://maps-dl-ru1.mapswithme.com/\"]"
+#define DEFAULT_URLS_JSON "[\"https://storage.googleapis.com/hikingbook-maps/\", \"https://cdn3.mapswithme.com/\", \"https://cdn2.mapswithme.com/\", \"https://v2s-2.mapswithme.com/\" \"https://v2s-1.mapswithme.com/\", \"https://maps-dl-us1.mapswithme.com/\", \"https://maps-dl-eu2.mapswithme.com/\", \"https://maps-dl-ru3.mapswithme.com/\", \"https://maps-dl-ru2.mapswithme.com/\", \"https://maps-dl-ru1.mapswithme.com/\"]"
 #define TRAFFIC_DATA_BASE_URL ""
 #define USER_BINDING_PKCS12 ""
 #define USER_BINDING_PKCS12_PASSWORD ""
