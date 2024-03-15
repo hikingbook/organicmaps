49b44e2d602b5548aff631ffb2faad2a93a42610
Riley <ring8255@gmail.com>
2024-03-15 17:59:14 +0800

    Add map style label in 'MWMMapDownloadDialog'.

--- iphone/Maps/Classes/Widgets/MWMMapDownloadDialogDelegate.h
+++ iphone/Maps/Classes/Widgets/MWMMapDownloadDialogDelegate.h
@@ -17,5 +17,6 @@
 -(void)downloadDialog:(MWMMapDownloadDialog *)downloadDialog updateNumDownloadedMapsLimitLabel:(UILabel *)numDownloadedMapsLimitLabel isDownloading:(BOOL)isDownloading isInQueue:(BOOL)isInQueue;
 -(MWMMapSource)downloadDialog:(MWMMapDownloadDialog *)downloadDialog mapSourceForCountry:(NSString *)countryId;
 -(void)downloadDialog:(MWMMapDownloadDialog *)downloadDialog presentMapStyle:(NSString *)countryId;
+-(void)downloadDialog:(MWMMapDownloadDialog *)downloadDialog updateMapStyleLabel:(UILabel *)mapStyleLabel downloadingMapForCountry:(NSString *)countryId;;
 
 @end
