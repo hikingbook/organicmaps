ce7cc53b61b0d68bab09e6be0100b0862f69da60
Zheng-Xiang <kf_ke@hikingbook.net>
2024-09-18 00:27:05 +0800

    Rollback code

--- android/app/src/main/java/app/organicmaps/base/BaseMwmFragment.java
+++ android/app/src/main/java/app/organicmaps/base/BaseMwmFragment.java
@@ -1,15 +1,9 @@
-/**
- * Author by robin, Date on 11/30/21.
- * Comment: Comment unused code
- */
-
 package app.organicmaps.base;
 
 import android.content.Context;
 
 import androidx.fragment.app.Fragment;
 
-import app.organicmaps.util.OrganicmapsFrameworkAdapter;
 import app.organicmaps.util.Utils;
 
 public class BaseMwmFragment extends Fragment implements OnBackPressListener
@@ -18,9 +12,7 @@ public class BaseMwmFragment extends Fragment implements OnBackPressListener
   public void onAttach(Context context)
   {
     super.onAttach(context);
-    if (OrganicmapsFrameworkAdapter.INSTANCE.getFragment() == null) OrganicmapsFrameworkAdapter.INSTANCE.setFragment(this);
-    Utils.detachFragmentIfCoreNotInitialized(context, OrganicmapsFrameworkAdapter.INSTANCE.getFragment());
-//    Utils.detachFragmentIfCoreNotInitialized(context, this);
+    Utils.detachFragmentIfCoreNotInitialized(context, this);
   }
 
   @Override
